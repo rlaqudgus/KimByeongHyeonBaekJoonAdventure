6763321ab2785e5d81addb9d4fc39d50ce37affd
rlaqudgus <108641304+rlaqudgus@users.noreply.github.com>
2022-12-12 19:51:27 +0900

    [Silver IV] Title: 스택, Time: 72 ms, Memory: 7152 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cs
+++ 백준/Silver/10828. 스택/스택.cs
@@ -6,68 +6,62 @@ using System.Threading.Tasks;
 
 namespace Codingcoding.Baekjoon
 {
-    internal class Bj_10828
+    internal class Test
     {
-        static List<int> myList = new List<int>();
         static StringBuilder sb = new StringBuilder();
-        static void Stack(string input)
+        static List<int> myList = new List<int>();
+
+        public static void Stack(string order)
         {
-            switch (input)
+            switch (order)
             {
                 case "pop":
-                    if (myList.Count == 0)
+                    if (myList.Count==0)
                     {
                         sb.AppendLine("-1");
                     }
                     else
                     {
-                        sb.AppendLine(myList[myList.Count - 1].ToString());
+                        int a = myList[myList.Count - 1];
+                        sb.AppendLine(a.ToString());
                         myList.RemoveAt(myList.Count - 1);
                     }
+                    
                     break;
-
                 case "size":
-                    sb.AppendLine(myList.Count.ToString());
+                    int b = myList.Count;
+                    sb.AppendLine(b.ToString());
                     break;
-
                 case "empty":
-                    int answer2 = myList.Count == 0 ? 1 : 0;
-                    sb.AppendLine(answer2.ToString());
+                    int c = myList.Count == 0 ? 1 : 0;
+                    sb.AppendLine(c.ToString());
                     break;
-
                 case "top":
-                    int answer3 = myList.Count == 0 ? -1 : myList[myList.Count - 1];
-                    sb.AppendLine(answer3.ToString());
+                    int d = myList.Count == 0 ? -1 : myList[myList.Count - 1];
+                    sb.AppendLine(d.ToString());
                     break;
             }
-
-                
         }
-        static void Stack(string input,int X)
-        {
-            myList.Add(X);
 
+        public static void Stack(string order, int num)
+        {
+            myList.Add(num);
         }
 
         public static void Main()
         {
-            //Input testcase N
+            int count = int.Parse(Console.ReadLine());
 
-            int N = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < N; i++)
+            for (int i = 0; i < count; i++)
             {
                 string[] input = Console.ReadLine().Split(" ");
 
                 if (input.Length == 2)
                 {
-                    Stack(input[0], int.Parse(input[1]));
+                    Stack("push", int.Parse(input[1]));
                 }
 
-                if (input.Length == 1)
-                {
-                    Stack(input[0]);
-                }
+                else Stack(input[0]);
             }
 
             Console.WriteLine(sb);
